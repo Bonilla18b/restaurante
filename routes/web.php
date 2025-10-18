62d1b396881170c026c8fed4a23428cc31d1f7ca
KevinSD5 <ksdelgadod@ufpso.edu.co>
2025-10-17 18:20:36 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*use App\Http\Controllers\UserController;
+Route::get('/users', [UserController::class, 'index']); */
+
+/*Route::get('/about', function () {return 'Acerca de nosotros';}); 
+
+Route::get('/user/{id}', function ($id) {return 'ID de usuario: ' . $id;});
+
+Route::get('/contacto', function () {return 'Página de contacto';})->name('contacto');
+
+Route::get('/user/{id}', function ($id) {return 'ID de usuario: ' . $id;})->where('id', '[0-9]{3}');
+
+Route::prefix('admin')->group(function () {
+Route::get('/', function () {return 'Panel de administración';});
+Route::get('/users', function () {return 'Lista de usuarios';});});/* 
+//--------------------------------------------------- Ejercicio---------------------------------------*/
+
+
+Route::get('/producto', function () {return 'Aqui productos';}); //cambiar esto
